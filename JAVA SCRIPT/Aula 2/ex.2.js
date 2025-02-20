463a130f545e5c433593a3c66d1ed6c9ef1b6430
William cavalcante <cavalcante.william@gmail.com>
2025-02-20 19:42:41 +0000

    1

---
+++ JAVA SCRIPT/Aula 2/ex.2.js
@@ -0,0 +1,23 @@
+var nome, sobrenome, idade, soma;
+var total;
+
+nome = "Dimitri";
+sobrenome = "Teixeira";
+idade = 30;
+soma = nome +" " +sobrenome;
+
+
+total = soma + " " + idade;
+alert ( total)
+
+
+var idade, eleitor;
+var idade = 19;
+eleitor = (idade < 18) ? "Nao, eleitor" : " Sim, eleitor ";
+alert('A resposta é ' + eleitor + 'a idade dele é ' + idade )
+
+
+
+
+
+
---
+++ JAVA SCRIPT/Aula 2/index.js
@@ -0,0 +1,3 @@
+document.getElementById("texto").innerHTML="Meu primeiro <b>texto</b>";
+alert(10 + 5)
+console.log("oi eu sou um console.log")
